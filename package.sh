453453fa5b63270411437dba960a1f81eea7820e
arlaneenalra <emeraldd.chris@gmail.com>
2011-10-27 23:32:05 -0500

    Builds a .deb but the deb is currently empty

--- package.sh
+++ package.sh
@@ -40,12 +40,35 @@ GIT_PATH=`which git`
 . ./project.conf
 
 # Validate that our required paramters are configured
-check_defined 'AUTHOR' $AUTHOR
+check_defined 'AUTHOR_EMAIL' $AUTHOR_EMAIL
 
-check_defined 'PROJECT_GIT_URL' $PROJECT_GIT_URL
+check_defined 'PROJECT_GIT_PATH' $PROJECT_GIT_PATH
 check_defined 'PROJECT_NAME' $PROJECT_NAME
 
 check_defined 'PACKAGING_ROOT' $PACKAGING_ROOT
 
+# validate that the PACKAGING_ROOT exists, create it if not
+if [ -e $PACKAGING_ROOT ]
+then
+	mkdir -p $PACKAGING_ROOT 	
+fi
+
+WORKING_DIR="$PACKAGING_ROOT/$PROJECT_NAME""-""$VERSION"
+
 # Build out the package dir
+git init $WORKING_DIR
+
+# Pull in code from our project 
+(
+	# sub shell to avoid lossing our current dir 
+	cd $PROJECT_GIT_PATH
+	GIT_WORK_TREE="$WORKING_DIR" git checkout -f
+)
+
+cd $WORKING_DIR
+
+dh_make --native -s --email $AUTHOR_EMAIL
+
+dpkg-buildpackage
+
 
